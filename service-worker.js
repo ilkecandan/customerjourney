8c27d80ab10db986774ce062c1eec85e3bac3024
İlke Candan Bengi <64421962+ilkecandan@users.noreply.github.com>
2025-04-11 23:34:31 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,9 +6,11 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.add(START_URL))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(START_URL))
+  );
 });
